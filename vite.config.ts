6eaa9806d33e4f3071255185940612eb2234895f
tanghaojie <thj_3360016@qq.com>
2022-12-12 16:10:04 +0800

    REFACTOR: update package

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,29 @@
 import { defineConfig, loadEnv } from 'vite'
-import { resolve } from 'path'
+import { resolve, dirname } from 'path'
 import cesium from 'vite-plugin-cesium'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
+import { fileURLToPath } from 'url'
 
 export default defineConfig(({ command, mode }) => {
   const alias = [{ find: '@', replacement: resolve(__dirname, 'src') }]
-  if (mode === 'development') {
-    alias['vue-i18n'] = 'vue-i18n/dist/vue-i18n.cjs.js'
-  }
+  // if (mode === 'development') {
+  //   alias['vue-i18n'] = 'vue-i18n/dist/vue-i18n.cjs.js'
+  // }
 
   return {
     plugins: [
       vue(),
       cesium(),
+      VueI18nPlugin({
+        /* options */
+        // locale messages resource pre-compile option
+        include: resolve(dirname(fileURLToPath(import.meta.url)), './path/to/src/locales/**'),
+      }),
       AutoImport({
         resolvers: [ElementPlusResolver()],
       }),
